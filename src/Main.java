3ada8c6c4a07ecd0da22710d28ccfc670ddc159a
shimon <shimon.on5@gmail.com>
2021-03-30 19:15:46 +0300

    break

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -40,8 +42,34 @@ public class Main {
     return SwapIndexes(AddRoundKey(aes_1,key1));
     }
 
-    public Byte[][] BreakEncryption(byte[][] message,byte[][] Ciphertext){
+    public void BreakEncryption(byte[][]message, byte[][] ciphertext, String output){
+        int len = message.length;
+        byte[][] cm = AddRoundKey(message,ciphertext);
+        byte[][] k2 = new byte[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                k2[i][j] = 1;
+            }
+        }
+        byte [][] swapK2 = AddRoundKey(cm,k2);
+        byte[][] k1 = SwapIndexes(swapK2);
+        try (FileOutputStream out = new FileOutputStream(output)) {
 
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    out.write(k1[i][j]);
+                }
+            }
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    out.write(k2[i][j]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
